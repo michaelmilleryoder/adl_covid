4fd8063e21e64d5bbb532a027e8038d2f1e111bc
Michael Miller Yoder <michaelmilleryoder@gmail.com>
2022-06-16 16:58:13 -0400

    anonymizing tweets

--- scripts/filter_process_tweets.py
+++ scripts/filter_process_tweets.py
@@ -1,5 +1,5 @@
 """ 
-Load, filter COVID Twitter data
+Load, filter, process COVID Twitter data
 By keywords and media attachments
 """
 
@@ -14,6 +14,41 @@ import json
 
 from tqdm import tqdm
 import pandas as pd
+
+
+def remove_mentions(text, user_mentions):
+    """ Remove mentions from a text """
+    new_text = text
+    usernames = [mention['screen_name'] for mention in user_mentions]
+    for username in usernames:
+        new_text = re.sub(username, 'USER', new_text, flags=re.IGNORECASE)
+    return new_text
+
+
+def process_text(text, user_mentions):
+    new_text = remove_mentions(text, user_mentions)
+    return ' '.join(new_text.split(' ')[:-1]) # remove URL
+
+
+def process_tweets(data):
+    """ Process a dataframe of tweets """
+    selected_cols = ['created_at', 'id_str', 'text', 'in_reply_to_status_id_str', 'geo', 'coordinates', 'place', 
+                 'is_quote_status', 'quote_count', 'reply_count', 'retweet_count', 'favorite_count', 'possibly_sensitive',
+                 'search_match',
+                 'user.id_str', 'user.location', 'user.geo_enabled',
+                 'entities.hashtags', 'entities.urls', 'entities.user_mentions', 'entities.symbols', 'entities.media', 'extended_entities.media',
+                 'retweeted_status.id_str', 'retweeted_status.user.location', 'retweeted_status.geo', 'retweeted_status.coordinates', 'retweeted_status.place', 
+                 'retweeted_status.quote_count', 'retweeted_status.reply_count', 'retweeted_status.retweet_count',
+                 'quoted_status.id_str', 'quoted_status.user.location', 'quoted_status.geo', 'quoted_status.coordinates', 'quoted_status.place', 
+                 'quoted_status.quote_count', 'quoted_status.reply_count', 'quoted_status.retweet_count',
+                ]
+    # Check if columns are present
+    selected_cols = [col for col in selected_cols if col in data.columns]
+    data = data[selected_cols].copy()
+    data['url'] = [el[-1] for el in data.text.str.split(' ')]
+    data['processed_text'] = [process_text(text, mentions) for text, mentions in zip(data['text'], data['entities.user_mentions'])]
+    data = data[sorted(data.columns)]
+    return data.drop(columns='text')
 
 
 class TweetFilter():
@@ -59,6 +94,25 @@ class TweetFilter():
                 
         return select
 
+    def load_process_tweets(self):
+        """ Load tweets that have been filtered and saved already.
+            Use if you want to do additional processing
+        """
+        filtered_dirpath = os.path.join('../output', 'tweets_json_nol')
+        out_dirpath = os.path.join('../output', 'processed_tweets_csv')
+        if not os.path.exists(out_dirpath):
+            os.mkdir(out_dirpath)
+        for fname in tqdm(sorted(os.listdir(filtered_dirpath)), ncols=80):
+            fpath = os.path.join(filtered_dirpath, fname)
+            with open(fpath) as f:
+                data = pd.json_normalize([json.loads(line) for line in f.read().splitlines()])
+            if not len(data) == 0:
+                processed = process_tweets(data)
+        
+            # Save out
+            outpath = os.path.join(out_dirpath, f'{os.path.splitext(fname)[0]}.csv')
+            processed.to_csv(outpath)
+
     def process_dump(self, fpath):
         """ Process a jsonlines dumped Twitter file """
         selected = []
@@ -87,7 +141,6 @@ class TweetFilter():
                 if match:
                     tweet['search_match'] = match
                     selected.append(tweet)
-                    self.n_selected += 1
                 # if i > 100:
 
         # Save out selected
@@ -97,7 +150,7 @@ class TweetFilter():
             pd.json_normalize(selected).to_csv(csv_outpath)
 
     def run(self):
-        self.load_resources()
+        #self.load_resources()
 
         # Load COVID Twitter data (Carley lab)
         # Older data
@@ -107,13 +160,14 @@ class TweetFilter():
         #    list(tqdm(p.imap(self.process_dump, paths), ncols=80, total=len(paths)))
 
         # Newer data
-        dirname = 'json_keyword_stream_mike'
-        paths = [os.path.join(self.basepath, dirname, fname) for fname in sorted(os.listdir(os.path.join(self.basepath, dirname))) if fname.endswith('json.gz')]
-        with Pool(15) as p:
-            list(tqdm(p.imap(self.process_dump, paths), ncols=80, total=len(paths)))
-        #list(tqdm(map(self.process_dump, paths), ncols=80, total=len(paths)))
+        #dirname = 'json_keyword_stream_mike'
+        #paths = [os.path.join(self.basepath, dirname, fname) for fname in sorted(os.listdir(os.path.join(self.basepath, dirname))) if fname.endswith('json.gz')]
+        #with Pool(15) as p:
+        #    list(tqdm(p.imap(self.process_dump, paths), ncols=80, total=len(paths)))
+        #list(tqdm(map(self.process_dump, paths), ncols=80, total=len(paths))) # debugging
+
+        self.load_process_tweets()
     
-        print(self.n_selected)
         #print(Counter([select['search_match'] for select in selected]).most_common())
 
 
